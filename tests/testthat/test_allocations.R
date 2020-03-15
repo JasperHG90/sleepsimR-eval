0c0483dbb43296813f801152e8965aa31196e0d4
Jasper Ginn <jasperginn@gmail.com>
2020-03-15 11:31:49 +0100

    Add test for avg runtime

--- tests/testthat/test_allocations.R
+++ tests/testthat/test_allocations.R
@@ -2,10 +2,13 @@ context("Test allocation methods and class")
 
 test_that("Can load allocations file from disk and convert to data frame",{
   # Load
-  allocs <- read_allocations_file("allocations.json")
+  allocs <- read_allocations_file("tests/testthat/allocations.json")
   expect_s3_class(allocs, "sleepsimR_allocations")
   expect_named(allocs, "abcd")
   expect_length(allocs, 1)
   expect_length(allocs[[1]], 4)
   expect_named(allocs[[1]], c("iteration_id", "ts_request", "ts_finished", "runtime_minutes"))
+  # Compute average runtime
+  ar <- round(average_runtime(allocs),1)
+  expect_equal(ar, 2.6)
 })
